3f68b09157c31b3cdbad14acc4d09d882c0613d1
Sara S Kjeldgaard <sara.kjeldgaard@gmail.com>
2020-10-19 17:41:05 +0200

    Corrections

--- js/logind.js
+++ js/logind.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     //we make the var popUp so we reuse it easily
-    var popUp = $("#popUpBackground");
+    var popUp = $("#popUpWrapper");
 
     //When we enter the page we hide popUp
     popUp.hide();
 
     //We fetch the button though jQuery and make a click event to show the payment pop-up when the button is clicked. 
-    $("#menuLogIn").click(function() {
+    $("#logInBtn").click(function() {
         popUp.show();
     })
     
--- js/profil.js
+++ js/profil.js
@@ -5,13 +5,15 @@ $(document).ready(function() {
     //When we enter the page we hide popUp
     popUp.hide();
 
-    //We fetch the button though jQuery and make a click event to show the payment pop-up when the button is clicked. 
+    //We fetch the button though jQuery and make a click event to show the payment pop-up when the button is clicked.
     $("#paymentDummy").click(function() {
+        // We do not want the user to be able to scroll the page when the popup is open
+        document.body.style.overflow = "hidden";
         popUp.show();
-    })
-    
-    
+    });
+
     $("#popUpActions").click(function() {
+        document.body.style.overflow = "visible";
         popUp.hide();
-    })
+    });
 });
